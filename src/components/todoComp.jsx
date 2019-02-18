b5eaf782b702186db80eb8d5617153bde2882b7d
tree <sunshukang30@163.com>
2019-02-18 10:01:15 +0800

    todo list -> todo & undo

--- src/components/todoComp.jsx
+++ src/components/todoComp.jsx
@@ -3,8 +3,8 @@ import { connect } from "react-redux";
 import { setVisibility } from "../reducers/todo";
 import { List, Avatar, Input } from "antd";
 
-import { requestTodos, requestAddTodo } from "../reducers/todo";
-import { bindActionCreators } from "../../../../../../Library/Caches/typescript/3.3/node_modules/redux";
+import { requestTodos, requestAddTodo, requestFinishTodo, requestUndoTodo } from "../reducers/todo";
+import { bindActionCreators } from "redux";
 class TodoComp extends Component {
   constructor(props) {
     super(props);
@@ -12,6 +12,8 @@ class TodoComp extends Component {
       input_todo: ""
     };
     this.addHandle = this.addHandle.bind(this);
+    this.undoHandle = this.undoHandle.bind(this);
+    this.doneHandle = this.doneHandle.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
@@ -31,56 +33,66 @@ class TodoComp extends Component {
     });
   }
 
+  undoHandle(id) {
+    const { requestUndoTodo } = this.props;
+    requestUndoTodo(id);
+  }
+
+  doneHandle(id) {
+    const { requestFinishTodo } = this.props;
+    requestFinishTodo(id);
+  }
+
   componentDidMount() {
     const { requestTodos } = this.props;
     requestTodos({ filter: "all" });
   }
 
   render() {
-    const { todos, visibilityFilter } = this.props;
+    const { todos } = this.props;
     const { input_todo } = this.state;
 
     const f_todos = todos.map(t => {
       var pref = ""
+      var displayComp = null
       if (t.state === 1) {
         pref  = `[未完成]`
+        displayComp = (
+          <div>
+            <button onClick={() => this.doneHandle(t.id)}>完成</button>
+            <a>{pref}{t.text}</a>
+          </div>
+        )
       } else if (t.state === 2) {
         pref = "[已完成]"
+        displayComp = (
+          <div>
+            <button onClick={() => this.undoHandle(t.id)}>撤销</button>
+            <s><a>{pref}{t.text}</a></s>
+          </div>
+        )
       } else if (t.state === 3) {
         pref = "[已删除]"
+        displayComp = (<s><a>{pref}{t.text}</a></s>)
       }
+      t.displayComp = displayComp
       t.pref = pref
       return t
-    })
-    var containerStyle = {
-      margin: "0.2rem",
-      padding: "0.5rem",
-      background: "rgb(255, 255, 255)"
-    };
-
-    const buttonStyle = {
-      width: "3rem",
-      float: "right",
-      marginTop: "0.5rem"
-    };
-
-    const listStyle = {
-      marginTop: "3rem"
-    };
+    }).reverse()
 
     return (
-      <div style={containerStyle}>
+      <div style={styles.containerStyle}>
         <Input
           style={{ marginTop: "0.5rem" }}
           placeholder="input new todo"
           value={input_todo}
           onChange={this.handleInputChange}
         />
-        <button style={buttonStyle} onClick={this.addHandle}>
+        <button style={styles.buttonStyle} onClick={this.addHandle}>
           添加
         </button>
         <List
-          style={listStyle}
+          style={styles.listStyle}
           itemLayout="vertical"
           size="large"
           pagination={{
@@ -95,7 +107,7 @@ class TodoComp extends Component {
           renderItem={item => (
             <List.Item key={item.text} actions={[]}>
               <List.Item.Meta
-                title={<a>{item.pref}{item.text}</a>}
+                title={item.displayComp}
                 avatar={<Avatar children={<a>{item.text[0]}</a>} />}
               />
             </List.Item>
@@ -103,6 +115,22 @@ class TodoComp extends Component {
         />
       </div>
     );
+  }
+}
+
+const styles = {
+  containerStyle: {
+    margin: "0.2rem",
+    padding: "0.5rem",
+    background: "rgb(255, 255, 255)"
+  },
+  buttonStyle: {
+    width: "3rem",
+    float: "right",
+    marginTop: "0.5rem"
+  },
+  listStyle: {
+    marginTop: "3rem"
   }
 }
 
@@ -114,7 +142,9 @@ const mapDispatchToProps = dispatch => {
   return {
     setVisibility: filter => dispatch(setVisibility(filter)),
     requestTodos: bindActionCreators(requestTodos, dispatch),
-    requestAddTodo: bindActionCreators(requestAddTodo, dispatch)
+    requestAddTodo: bindActionCreators(requestAddTodo, dispatch),
+    requestFinishTodo: bindActionCreators(requestFinishTodo, dispatch),
+    requestUndoTodo: bindActionCreators(requestUndoTodo, dispatch)
   };
 };
 
