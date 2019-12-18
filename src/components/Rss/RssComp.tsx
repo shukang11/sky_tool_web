7dee3d85430638de055632269c0ec1aa1a4b62e8
tree <2332532718@qq.com>
2019-12-18 15:38:56 +0800

    <feature>: <Rss>
    <body>
    订阅
    <footer>

--- src/components/Rss/RssComp.tsx
+++ src/components/Rss/RssComp.tsx
@@ -1,22 +1,36 @@
 import * as React from "react";
-import PageLoading from "../PageLoading/index";
 import { connect } from "react-redux";
 import { Button, List, message, Empty, Modal, Form, Input } from "antd";
 import { rssSourceList, addRssLink } from "./../../services/rss";
-import { IRss, IRssModel, add_rss_action } from "src/reducers/rss";
+import "./RssComp.scss";
+
 const FormItem = Form.Item;
+
+export declare interface IRssModel {
+  id: number;
+  link: string;
+  title: string;
+  description: string;
+}
 
 type IRssProps = Readonly<{
   form: any;
-  addRsses: Function;
 }>;
 
 interface IRssState {
+  rsses: Array<IRssModel>;
   isShowAddRssLink: boolean;
+  pages: number;
+  limit: number;
+  hasMore: boolean;
 }
 class RssComp extends React.Component<IRssProps, IRssState> {
   state = {
-    isShowAddRssLink: false
+    rsses: [],
+    isShowAddRssLink: false,
+    pages: 0,
+    limit: 11,
+    hasMore: true
   };
   /* 点击显示对话框 */
   onWantCreateRssLinkClicked = () => {
@@ -30,75 +44,152 @@ class RssComp extends React.Component<IRssProps, IRssState> {
       message.error("链接不可为空");
       return;
     }
-    
+
     addRssLink(link).then(r => {
       if (!r || !r.data) {
         return;
       }
-      console.log(r.data);
+      this.fetchRssLimit();
     });
     this.setState({ isShowAddRssLink: false });
   };
+
+  fetchRssLimit() {
+    const { pages, limit } = this.state;
+    rssSourceList(pages, limit).then(r => {
+      if (!r || !r.data) {
+        return;
+      }
+      if (r.data.length === 0) {
+        return;
+      }
+      var append: Array<IRssModel> = r.data.map(
+        (i: { rss_id: any; rss_link: any; rss_title: any }) => ({
+          id: i.rss_id,
+          link: i.rss_link,
+          title: i.rss_title,
+          description: ""
+        })
+      );
+      if (append.length < this.state.limit) {
+        this.setState({ hasMore: false });
+      }
+      this.setState({
+        rsses: this.state.rsses.concat(append)
+      });
+    });
+  }
+
+  componentDidMount() {
+    if (this.state.rsses.length === 0) {
+      this.fetchRssLimit();
+    }
+  }
+
   render() {
     const {
-      form: { getFieldDecorator }
+      form: { getFieldDecorator },
     } = this.props;
+    const {rsses} = this.state;
+    
+    const ModalItem = (
+      <Modal
+        visible={this.state.isShowAddRssLink}
+        onOk={() => {
+          this.onCreateRssLinkAction();
+        }}
+        onCancel={() => {
+          this.setState({ isShowAddRssLink: false });
+        }}
+        okText={"确认"}
+        cancelText={"取消"}
+      >
+        <Form>
+          <FormItem label="链接地址">
+            {getFieldDecorator("link", {
+              rules: [
+                {
+                  require: true,
+                  message: "rss link is required!"
+                }
+              ]
+            })(<Input placeholder="请输入订阅源" />)}
+          </FormItem>
+        </Form>
+      </Modal>
+    );
+    if (!rsses || rsses.length == 0) {
+      return (
+        <div>
+          <Empty
+            className="empty"
+            image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+            imageStyle={{
+              height: 160
+            }}
+            description={<span>暂无订阅源</span>}
+          >
+            <Button
+              type="primary"
+              onClick={() => {
+                this.onWantCreateRssLinkClicked();
+              }}
+            >
+              创建一个
+            </Button>
+          </Empty>
+          {ModalItem}
+        </div>
+      );
+    }
+
+    const data = rsses;
     return (
       <div>
-        <Empty
-          className="empty"
-          image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
-          imageStyle={{
-            height: 160
-          }}
-          description={<span>暂无订阅源</span>}
-        >
-          <Button
-            type="primary"
-            onClick={() => {
-              this.onWantCreateRssLinkClicked();
-            }}
-          >
-            创建一个
-          </Button>
-        </Empty>
-        <Modal
-          visible={this.state.isShowAddRssLink}
-          onOk={() => {
-            this.onCreateRssLinkAction();
-          }}
-          onCancel={() => {
-            this.setState({ isShowAddRssLink: false });
-          }}
-          okText={"确认"}
-          cancelText={"取消"}
-        >
-          <Form>
-            <FormItem label="链接地址">
-              {getFieldDecorator("link", {
-                rules: [
-                  {
-                    require: true,
-                    message: "rss link is required!"
-                  }
-                ]
-              })(<Input placeholder="请输入订阅源" />)}
-            </FormItem>
-          </Form>
-        </Modal>
+        <List
+          header={
+            <div className="center-has-more">
+              <Button
+                type="primary"
+                onClick={() => {
+                  this.onWantCreateRssLinkClicked();
+                }}
+              >
+                创建一个
+              </Button>
+            </div>
+          }
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                title={item.title}
+                description={item.link}
+              ></List.Item.Meta>
+            </List.Item>
+          )}
+          loadMore={
+            this.state.hasMore ? (
+              <div className="center-has-more">
+                <Button
+                  onClick={() => {
+                    this.setState({ pages: this.state.pages + 1 });
+                    this.fetchRssLimit();
+                  }}
+                >
+                  加载更多
+                </Button>
+              </div>
+            ) : (
+              <div></div>
+            )
+          }
+        ></List>
+        {ModalItem}
       </div>
     );
   }
 }
-
-const mapStateToProps = (state: {rss: IRss}) => {
-  return {rsses: state.rss.rsses};
-}
-
-const mapDispatchToProps = (dispatch: any) => ({
-  addRsses: (rsses: Array<IRssModel>) => {
-    dispatch(add_rss_action(rsses))
-  }
-})
 
 export default connect()(Form.create<IRssProps>()(RssComp));
